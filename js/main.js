5bdf0a85357c14d32e4edd49fe22f09cf7646da9
Julien Chichignoud <jchichignoud@gmail.com>
2018-04-09 22:08:16 +1000

    add close button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, window, location, CSInterface, SystemPath, themeManager*/
 
+
 (function () {
     'use strict';
 
@@ -10,6 +11,21 @@
         $("#btn_projects").click(function() {
             var projectsList = csInterface.evalScript("getProjectsList()", updateProjectsList);
           });
+
+          $('#projects').bind('click', function(event) {
+            if (event.target.nodeName === "LI") {
+                console.log("list item");
+            } else if (event.target.nodeName === "P") {
+                console.log("name");
+            } else if (event.target.nodeName === "IMG") {
+                console.log("logo");
+            } else if (event.target.nodeName ===  "svg" || event.target.nodeName === "path") {
+                console.log($(event.target).closest('li').attr('id') + " from SVG or path");
+                var projectID = $(event.target).closest('li').attr('id');
+                csInterface.evalScript("removeProject(" + projectID + ")");
+                csInterface.evalScript("getProjectsList()", updateProjectsList);
+            }
+         });
     }
         
     init();
--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -5,7 +5,11 @@
 function getProjectsList() {
     var projectsToHtml = ""
     for ( i = 0; i < app.projects.numProjects; i++){
-        projectsToHtml = projectsToHtml + "<li>" + app.projects[i].rootItem.name + "</li>"
+        projectsToHtml = projectsToHtml + "<li id='" + i + "'><img src='icons/SP_NonMedia_Sm_N_D@3to2x.png'><p>" + (app.projects[i].rootItem.name).slice(0, -7) + "</p><i class='fas fa-times'></i></li>"
     }
     return projectsToHtml;
+}
+
+function removeProject(id) {
+    app.projects[id].closeDocument()
 }
